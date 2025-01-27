efa66e2ee70e41a06881aa20511e449700325f59
Alejandro Restrepo Klinge <56825548+mrrestre@users.noreply.github.com>
2025-01-27 18:52:01 +0100

    added solution

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,103 @@
 use std::{collections::HashSet, fs};
 
-/* 
-Eure Aufgabe: Ihr wollt einen Spam-Filter für eingehende Emails schreiben, basierend auf einer Liste bekannter Spam-Domains. Vervollständigt dafür die vorgegebenen Funktionen! Optional dürft ihr gerne die Unit-Tests nutzen.
+const EMAIL_PATH : &str = "./resources/emails.txt";
+const BLACKLIST_PATH : &str = "./resources/blacklist.txt";
 
-- Die zu testenden Emails sowie die Liste der Spam-Domains sollen aus dem Ressourcen-Verzeichnis eingelesen werden.
-- Die Textdateien dürfen nicht verändert werden!
-- Mail-Adressen sollen genau dann als Spam erkannt werden, wenn ihre Domain-Endung mit einer Regel >übereinstimmt<.
-
-Beispiel: 'fastmoney.biz' -> 'luckywinner9000@fastmoney.biz' ist Spam
-
-Viel Spaß!
- */
 fn is_spam(mail: &str, blacklist: &HashSet<String>) -> bool {
-    todo!("true falls mail als Spam erkannt wurde")
+    for spam_domain in blacklist  {
+        let temp = format!("@{}", spam_domain);
+        if mail.ends_with(temp.as_str()) {
+            return true;
+        }
+    }
+    return false;
 }
 
-fn load_blacklist(path: &str) -> Result<HashSet<String>, std::io::Error> {
-    todo!("liefert Set von bekannten Spam-Domains aus 'resources\\blacklist.txt'")
+fn load_file(path: &str) -> Result<HashSet<String>, std::io::Error> {
+    let contents = fs::read_to_string(path)?;
+
+    let mut hashmap = HashSet::new();
+    for line in contents.lines() {
+        hashmap.insert(line.to_string());
+    }
+
+    Ok(hashmap)
 }
 
 fn main() {
-    todo!("Finde alle 15 Spam-Adressen in 'resources\\emails.txt'")
+    let emails = match load_file(&EMAIL_PATH){
+        Ok(hash_set) => hash_set,
+        Err(error) => panic!("{:?}", error),
+    };
+
+    let blacklist = match load_file(&BLACKLIST_PATH){
+        Ok(hash_set) => hash_set,
+        Err(error) => panic!("{:?}", error),
+    };
+
+    for email in emails {
+        let res = is_spam(email.as_str(), &blacklist);
+        if res {
+            println!("Spam {:?}", email)
+        }
+    }
 }
 
-/* 
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_load_blacklist() {
-        assert_eq!(blacklist, 11);
+        let blacklist = match load_file(&BLACKLIST_PATH){
+            Ok(hash_set) => hash_set,
+            Err(error) => panic!("{:?}", error),
+        };
+        assert_eq!(blacklist.len(), 11);
+    }
+
+    #[test]
+    fn test_load_email() {
+        let email = match load_file(&EMAIL_PATH){
+            Ok(hash_set) => hash_set,
+            Err(error) => panic!("{:?}", error),
+        };
+        assert_eq!(email.len(), 100);
     }
 
     #[test]
     fn test_is_spam() {
-        assert_eq!(spam_count, 15);
+        let mut test_blacklist: HashSet<String> = HashSet::new();
+        test_blacklist.insert("scamalerts.biz".to_string());
+        test_blacklist.insert("fishyclickbait.biz".to_string());
+
+        assert_eq!(is_spam("scamalerts.biz", &test_blacklist), false);
+        assert_eq!(is_spam("@scamalerts.biz", &test_blacklist), true);
+        assert_eq!(is_spam("test@scamalerts.biz", &test_blacklist), true);
+        assert_eq!(is_spam("test@totallyscam.blob", &test_blacklist), false);
+
+    }
+
+    #[test]
+    fn test_all() {
+        let blacklist = match load_file(&BLACKLIST_PATH){
+            Ok(hash_set) => hash_set,
+            Err(error) => panic!("{:?}", error),
+        };
+
+        let emails = match load_file(&EMAIL_PATH){
+            Ok(hash_set) => hash_set,
+            Err(error) => panic!("{:?}", error),
+        };
+        let mut counter = 0;
+        for email in emails {
+            let res = is_spam(email.as_str(), &blacklist);
+            if res {
+                counter += 1 ;
+            }
+        }
+
+        assert_eq!(counter, 15);
     }
 } 
-*/
